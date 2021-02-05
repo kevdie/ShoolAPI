3d369251d8566eb06f54552d99383fc76d670526
kevdie <kevindiegofloresantezana@hotmail.com>
2021-02-04 23:30:28 -0400

    add models

---
+++ SchooleAPI/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchooleAPI.Models
+{
+    public class Class
+    {
+        public int IdClass { get; set; }
+        [Required]
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
---
+++ SchooleAPI/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchooleAPI.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        
+        public IEnumerable<Class> classes { get; set; }
+
+    }
+}
